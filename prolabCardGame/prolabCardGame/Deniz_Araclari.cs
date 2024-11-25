b1aa4487602e3ca3e841fc913603188ae999a4fd
Merve Sevim <mervesevim050@gmail.com>
2024-11-25 17:37:15 +0300

    first upload

---
+++ prolabCardGame/prolabCardGame/Deniz_Araclari.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal abstract class Deniz_Araclari : Savas_Araclari
+    {
+
+        public int Seviye_Puani { get; set; } = 0;
+        public override int Dayaniklilik { get; set; }
+
+        public override int Vurus { get;  }
+
+        public override string Sinif { get; set; } = "Deniz";
+
+    
+
+        public abstract int HavaVurusAvantaji { get; set; }  
+
+        public abstract string AltSinif { get; set; }
+
+
+        public Deniz_Araclari(int seviyePuan, string sinif) : base(seviyePuan)
+        {
+
+
+        }
+        public override void DurumGuncelle()
+        {
+
+        }
+
+
+        public override void KartPuaniGoster()
+        {
+            base.KartPuaniGoster();
+        }
+    }
+}
